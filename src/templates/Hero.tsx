d2fa5fa92b71092be0cd77efa4fa42d28b6703f8
Gandha Ryanto <ganda@mdd.co.id>
2024-02-18 14:40:39 +0700

    Fix error

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-import { Background } from "../background/Background";
-import { Section } from "../layout/Section";
-import { NavbarTwoColumns } from "../navigation/NavbarTwoColumns";
-import { Logo } from "./Logo";
+import { Background } from '../background/Background';
+import { Section } from '../layout/Section';
+import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
+import { Logo } from './Logo';
 
 const Hero = () => (
   <Background color="bg-gray-100">
